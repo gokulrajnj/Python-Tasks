79147cee94f37d0d2130bdd6ffd5714e539cae6c
gokulrajnj <gokulrajnj@gmail.com>
2025-08-21 23:55:34 +0530

    Add files via upload

--- Task 2.py
+++ Task 2.py
@@ -1,3 +1,3 @@
 a=input("Enter your first name: ")
-b=input("Enter your first name: ")
-print(a,b,"! welcome to the python program.")
+b=input("Enter your last name: ")
+print("Hello,",a,b+"! welcome to the python program.")
